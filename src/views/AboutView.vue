d3d3c02c28f1f4ffad20d45012b7b0f3cd2f047a
Md Sharif Foysal Shoron <mdshariffoysalshoron@gmail.com>
2022-04-12 17:01:34 +0600

    Complete Website

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <AboutCom />
+  <FooterCom />
 </template>
+
+<script>
+// @ is an alias to /src
+import AboutCom from '@/components/AboutCom.vue'
+import FooterCom from '@/components/FooterCom.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    AboutCom,
+    FooterCom
+  },
+  mounted() {  
+      document.title = "Gyru | About";
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+  }
+}
+</script>
+
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'Gyru App',
+    shortName: 'Gyru',
+    themeColor: '#000000',
+    msTileColor: '#f7c434'
+  }
 })
